7ec4a43a380e8a2b1eea5badd460352bb157c9bf
DominikMistera <DominikMistera@DESKTOP-CL3ESFE>
2020-06-03 18:25:47 +0200

    Callback redundancy

--- Zad10/my-app/src/ToDoList/ToDoList.js
+++ Zad10/my-app/src/ToDoList/ToDoList.js
@@ -6,23 +6,12 @@ class ToDoList extends React.Component {
     constructor(props) {
         super(props);
 
-        this.handleCheck = this.handleCheck.bind(this);
-        this.handleUncheck = this.handleUncheck.bind(this);
     }
-
-    handleCheck(index) {
-        this.props.onCheck(index);
-    }
-
-    handleUncheck(index) {
-        this.props.onUncheck(index);
-    }
-
     render() {
         let tasks = this.props.tasks.reduce((accumulator, task, index) => {
             if(!this.props.filter || !task.checked) {
                 accumulator.push(
-                    <Task key={index} name={task.name} checked={task.checked} id={index} onCheck={this.handleCheck} onUncheck={this.handleUncheck}/>
+                    <Task key={index} name={task.name} checked={task.checked} id={index} onCheck={this.props.onCheck} onUncheck={this.props.onUncheck}/>
                 )
             }
             return accumulator;
